d7db09c1fe679c5d793553365533e9f949ebb329
starscy <starscy@rambler.ru>
2023-09-02 23:11:21 +0300

    repair vacancySeed

--- GoodJobs/database/seeders/DatabaseSeeder.php
+++ GoodJobs/database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            VacancySeeder::class, 
-            CategorySeeder::class
+            VacancySeeder::class,
+           CategorySeeder::class
         ]);
     }
 }
