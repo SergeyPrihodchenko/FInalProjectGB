a81f8d71165b948f15572bd1016075ef5ec35bcf
VladislavDeveloper <studentredin323@gmail.com>
2023-09-07 13:10:44 +0300

    front: edit profile info, change password and account delete

--- GoodJobs/resources/js/Pages/ProfilePage/ProfilePage.jsx
+++ GoodJobs/resources/js/Pages/ProfilePage/ProfilePage.jsx
@@ -5,40 +5,43 @@ import MainLayout from "@/Layouts/MainLayout/MainLayout";
 import { BtnDelete } from '@/Shared/ProfileButton/BtnDeleteProfilePage';
 import AppText from '@/Shared/ui/AppText/AppText';
 import { AuthContext } from '@/Shared/store/AuthContext';
+import { usePage, useForm } from '@inertiajs/react';
+import UpdatePasswordForm from './Forms/UpdatePasswordForm';
+import DeleteProfileForm from './Forms/DeleteProfileForm';
 
 
 
-const arrayForm = [{
-    lable: 'Имя',
-    placeholder: 'Иванов Иван',
-    type: 'text',
-},
-{
-    lable: 'Пароль',
-    placeholder: 'Обновлен 2 года назад',
-    type: 'password',
-},
-{
-    lable: 'Email',
-    placeholder: 'IvanovIvan@pirozok.com',
-    type: 'email',
-},
-{
-    lable: 'Мобильный телефон',
-    placeholder: '+7 (999) 777-88-99',
-    type: 'tel',
-},
-{
-    lable: 'Район поиска работы',
-    placeholder: 'Москва',
-    type: 'address',
-},
-{
-    lable: 'Соц сети',
-    placeholder: 'Не привязаны',
-    type: 'image',
-},
-];
+// const arrayForm = [{
+//     lable: 'Имя',
+//     placeholder: 'Иванов Иван',
+//     type: 'text',
+// },
+// {
+//     lable: 'Пароль',
+//     placeholder: 'Обновлен 2 года назад',
+//     type: 'password',
+// },
+// {
+//     lable: 'Email',
+//     placeholder: 'IvanovIvan@pirozok.com',
+//     type: 'email',
+// },
+// {
+//     lable: 'Мобильный телефон',
+//     placeholder: '+7 (999) 777-88-99',
+//     type: 'tel',
+// },
+// {
+//     lable: 'Район поиска работы',
+//     placeholder: 'Москва',
+//     type: 'address',
+// },
+// {
+//     lable: 'Соц сети',
+//     placeholder: 'Не привязаны',
+//     type: 'image',
+// },
+// ];
 
 const arrayNav = [
     'Личные данные',
@@ -48,7 +51,24 @@ const arrayNav = [
 ];
 
 function ProfilePage({ auth }) {
-    const user = auth?.user;
+
+    const user = usePage().props.auth.user;
+
+    const { data, setData, post, processing } = useForm({
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+        social_media: user.social_media,
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route('profile.update'));
+    }
+
+
     return (
         //<div className={s.container}>
         <AuthContext.Provider value={{ user }}>
@@ -76,21 +96,95 @@ function ProfilePage({ auth }) {
                                 </ul>
                             </div>
                             {
-                                arrayForm.map((el) => {
-                                    console.log(el);
-                                    return (
-                                        <form action="#" className={s.formProfilePage}>
-                                            <div className={s.formProfile}>
-                                                <lable for="name" className={s.textForm}>{el.lable}</lable>
-                                                <input className={s.inputFormProfile} type="text" placeholder={el.placeholder} />
-                                                <input className={s.btnFormProfile} type="submit" value="Изменить" />
-                                            </div>
-                                        </form>
-                                    )
-                                })
+                                // arrayForm.map((el) => {
+                                //     console.log(el);
+                                //     return (
+                                //         <form action="#" className={s.formProfilePage}>
+                                //             <div className={s.formProfile}>
+                                //                 <lable for="name" className={s.textForm}>{el.lable}</lable>
+                                //                 <input className={s.inputFormProfile} type="text" placeholder={el.placeholder} />
+                                //                 <input className={s.btnFormProfile} type="submit" value="Изменить" />
+                                //             </div>
+                                //         </form>
+                                //     )
+                                // })
                             }
 
-                            <BtnDelete />
+                            <form onSubmit={submit} className={s.formProfilePage}>
+                                <div className={s.formProfile}>
+                                    <lable for="name" className={s.textForm}>Имя:</lable>
+                                    <input
+                                        value={data.name} 
+                                        className={s.inputFormProfile} 
+                                        type="text" 
+                                        placeholder={data.name} 
+                                        onChange={(e) => setData('name', e.target.value)}
+                                    />
+                                    <input className={s.btnFormProfile} type="submit" value="Изменить" />
+                                </div>
+                            </form>
+
+                            <form onSubmit={submit} className={s.formProfilePage}>
+                                <div className={s.formProfile}>
+                                    <lable for="email" className={s.textForm}>Email:</lable>
+                                    <input
+                                        value={data.email} 
+                                        className={s.inputFormProfile} 
+                                        type="email" 
+                                        placeholder={data.email} 
+                                        onChange={(e) => setData('email', e.target.value)}
+                                    />
+                                    <input className={s.btnFormProfile} type="submit" value="Изменить" />
+                                </div>
+                            </form>
+
+                            <form onSubmit={submit} className={s.formProfilePage}>
+                                <div className={s.formProfile}>
+                                    <lable for="phone" className={s.textForm}>Телефон:</lable>
+                                    <input 
+                                        value={data.phone} 
+                                        className={s.inputFormProfile} 
+                                        type="phone" 
+                                        placeholder={data.phone} 
+                                        onChange={(e) => setData('phone', e.target.value)}
+                                    />
+                                    <input className={s.btnFormProfile} disabled={processing} type="submit" value="Изменить" />
+                                </div>
+                            </form>
+
+                            <form onSubmit={submit} className={s.formProfilePage}>
+                                <div className={s.formProfile}>
+                                    <lable for="address" className={s.textForm}>Адрес:</lable>
+                                    <input 
+                                        value={data.address}
+                                        className={s.inputFormProfile} 
+                                        type="text" 
+                                        placeholder={data.address} 
+                                        onChange={(e) => setData('address', e.target.value)}
+                                    />
+                                    <input className={s.btnFormProfile} type="submit" value="Изменить" />
+                                </div>
+                            </form>
+
+                            <form onSubmit={submit} className={s.formProfilePage}>
+                                <div className={s.formProfile}>
+                                    <lable for="social_media" className={s.textForm}>Социальные сети:</lable>
+                                    <input
+                                        value={data.social_media} 
+                                        className={s.inputFormProfile} 
+                                        type="text" 
+                                        placeholder={data.social_media} 
+                                        onChange={(e) => setData('social_media', e.target.value)}
+                                    />
+                                    <input className={s.btnFormProfile} type="submit" value="Изменить" />
+                                </div>
+                            </form>
+
+
+
+                            <UpdatePasswordForm />
+
+                            <DeleteProfileForm />
                             {/* <div className={s.btnProfilePage}>
           <button className={s.btnDeleteProfile} type="submit">Удаление аккаунта</button>
         </div> */}
