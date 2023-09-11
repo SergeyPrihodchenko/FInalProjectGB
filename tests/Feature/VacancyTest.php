87073cec22a8c568192da278767b149b59b35c5b
LibertyCode <libertycodeg@gmail.com>
2023-09-11 22:57:18 +0300

    rm P

--- tests/Feature/VacancyTest.php
+++ tests/Feature/VacancyTest.php
@@ -22,33 +22,37 @@ class VacancyTest extends TestCase
 
     public function test_index_return_page_is_vacancy_index(): void
     {
-        $this->get(route('vacancy.index'))->assertInertia(fn(Assert $page) => $page
-            ->component('Vacancy/Index')
+        $this->get(route('vacancy.index'))->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Vacancy/Index')
         );
     }
 
     public function test_can_view_vacancy_detail()
     {
         $this->get(route('vacancy.show', $this->vacancy->id))
-            ->assertInertia(fn(Assert $page) => $page
-                ->component('Vacancy/Show')
-                ->has('vacancy', fn(Assert $page) => $page
-                    ->where('id', $this->vacancy->id)
-                    ->where('title', $this->vacancy->title)
-                    ->where('payment', $this->vacancy->payment)
-                    ->where('employment', $this->vacancy->employment)
-                    ->where('description', $this->vacancy->description)
-                    ->where('experience', $this->vacancy->experience)
-                    ->where('contacts',  $this->vacancy->contacts)
-                    ->where('requirements',  $this->vacancy->requirements)
-                    ->where('responsibilities',  $this->vacancy->responsibilities)
-                    ->where('conditions',  $this->vacancy->conditions)
-                    ->where('skills',  $this->vacancy->skills)
-                    ->where('reviews',  $this->vacancy->reviews)
-                    ->where('company_id',  $this->vacancy->company_id)
-                    //datetime
-                    ->etc()
-                )
+            ->assertInertia(
+                fn (Assert $page) => $page
+                    ->component('Vacancy/Show')
+                    ->has(
+                        'vacancy',
+                        fn (Assert $page) => $page
+                            ->where('id', $this->vacancy->id)
+                            ->where('title', $this->vacancy->title)
+                            ->where('payment', $this->vacancy->payment)
+                            ->where('employment', $this->vacancy->employment)
+                            ->where('description', $this->vacancy->description)
+                            ->where('experience', $this->vacancy->experience)
+                            ->where('contacts',  $this->vacancy->contacts)
+                            ->where('requirements',  $this->vacancy->requirements)
+                            ->where('responsibilities',  $this->vacancy->responsibilities)
+                            ->where('conditions',  $this->vacancy->conditions)
+                            ->where('skills',  $this->vacancy->skills)
+                            ->where('reviews',  $this->vacancy->reviews)
+                            ->where('company_id',  $this->vacancy->company_id)
+                            //datetime
+                            ->etc()
+                    )
             );
     }
     public function test_can_create_a_new_vacancy()
@@ -71,31 +75,34 @@ class VacancyTest extends TestCase
         $vacancy = Vacancy::where('title', 'tester create')->get();
 
         $this->get(route('vacancy.show', $vacancy[0]->id))
-            ->assertInertia(fn(Assert $page) => $page
-                ->component('Vacancy/Show')
-                ->has('vacancy', fn(Assert $page) => $page
-                    ->where('id', $vacancy[0]->id)
-                    ->where('title', $vacancy[0]->title)
-                    ->where('payment', $vacancy[0]->payment)
-                    ->where('employment', $vacancy[0]->employment)
-                    ->where('description', $vacancy[0]->description)
-                    ->where('experience', $vacancy[0]->experience)
-                    ->where('contacts',  $vacancy[0]->contacts)
-                    ->where('requirements',  $vacancy[0]->requirements)
-                    ->where('responsibilities',  $vacancy[0]->responsibilities)
-                    ->where('conditions',  $vacancy[0]->conditions)
-                    ->where('skills',  $vacancy[0]->skills)
-                    ->where('reviews',  $vacancy[0]->reviews)
-                    ->where('company_id',  $vacancy[0]->company_id)
-                    //datetime
-                    ->etc()
-                )
+            ->assertInertia(
+                fn (Assert $page) => $page
+                    ->component('Vacancy/Show')
+                    ->has(
+                        'vacancy',
+                        fn (Assert $page) => $page
+                            ->where('id', $vacancy[0]->id)
+                            ->where('title', $vacancy[0]->title)
+                            ->where('payment', $vacancy[0]->payment)
+                            ->where('employment', $vacancy[0]->employment)
+                            ->where('description', $vacancy[0]->description)
+                            ->where('experience', $vacancy[0]->experience)
+                            ->where('contacts',  $vacancy[0]->contacts)
+                            ->where('requirements',  $vacancy[0]->requirements)
+                            ->where('responsibilities',  $vacancy[0]->responsibilities)
+                            ->where('conditions',  $vacancy[0]->conditions)
+                            ->where('skills',  $vacancy[0]->skills)
+                            ->where('reviews',  $vacancy[0]->reviews)
+                            ->where('company_id',  $vacancy[0]->company_id)
+                            //datetime
+                            ->etc()
+                    )
             );
     }
 
-    public function test_can_delete_vacancy ()
+    public function test_can_delete_vacancy()
     {
-        $this->delete(route('vacancy.destroy' , $this->vacancy->id));
+        $this->delete(route('vacancy.destroy', $this->vacancy->id));
 
         $this->assertDatabaseMissing('vacancies', [
             'id' => $this->vacancy->id,
@@ -103,10 +110,11 @@ class VacancyTest extends TestCase
         ]);
     }
 
-    public function test_vacancy_create_page_is_working ()
+    public function test_vacancy_create_page_is_working()
     {
-        $this->get(route('vacancy.create'))->assertInertia(fn(Assert $page) => $page
-            ->component('Vacancy/CreateVacancy')
+        $this->get(route('vacancy.create'))->assertInertia(
+            fn (Assert $page) => $page
+                ->component('Vacancy/CreateVacancy')
         );
     }
 }
