894f9b83b6a7a2184eaaa2de560e43e2587b8008
Foxsiny <mitiashkinata8@gmail.com>
2023-09-15 20:56:43 +0300

    add Company/Index.jsx

--- resources/js/Pages/Company/company_detail.jsx
+++ resources/js/Pages/Company/company_detail.jsx
@@ -1,15 +1,20 @@
 import MainLayout from "@/5Layouts/MainLayout/MainLayout";
-import { AppCard } from "@/8Shared/AppCard/AppCard";
+//import { AppCard } from "@/8Shared/ui/AppCard/AppCard";
 import { AppPage } from "@/5Layouts/AppPage/AppPage";
 import { Typography } from "@/8Shared/Typography/Typography";
 import React, { useState } from "react";
+
+
+
+
+
 
 const Company = ({ company }) => {
     console.log("company", company);
     return (
         <MainLayout>
             <AppPage>
-                <Typography variant={"h2"}>Компания</Typography>
+                <Typography variant={"h2"}>Здесь отображается конкретная компания</Typography>
             </AppPage>
         </MainLayout>
     );
