d13596f6c39640b19e55aba63c523bfeca918f22
LibertyCode <libertycodeg@gmail.com>
2023-09-16 23:50:42 +0300

    replenish ui

---
+++ resources/js/8Shared/ui/StarRating/StarRating.jsx
@@ -0,0 +1,66 @@
+import { memo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { classNames } from "@/shared/lib/classNames/classNames";
+
+import s from "./StarRating.module.scss";
+import { AppIcon as AppIconDeprecated } from "../../deprecated/AppIcon/AppIcon";
+
+import StarIcon from "@/shared/assets/icons/star.svg";
+import { ToggleFeatures } from "@/shared/lib/features";
+import { AppIcon } from "../AppIcon";
+
+const stars = [1, 2, 3, 4, 5];
+
+export const StarRating = memo((props) => {
+    const { className, onSelect, size = 30, selectedStars = 0 } = props;
+    const { t } = useTranslation();
+    const [currentStarsCount, setCurrentStarsCount] = useState(selectedStars);
+    const [isSelected, setIsSelected] = useState(Boolean(selectedStars));
+    const onHover = (starsCount) => () => {
+        if (!isSelected) {
+            setCurrentStarsCount(starsCount);
+        }
+    };
+
+    const onLeave = () => {
+        if (!isSelected) {
+            setCurrentStarsCount(0);
+        }
+    };
+
+    const onClick = (starsCount) => () => {
+        if (!isSelected) {
+            onSelect?.(starsCount);
+            setCurrentStarsCount(starsCount);
+            setIsSelected(true);
+        }
+    };
+    return (
+        <div className={classNames(s.starRatingRedesigned, {}, [className])}>
+            {stars.map((starNumber) => {
+                return (
+                    <AppIcon
+                        className={classNames(
+                            s.starIcon,
+                            { [s.selected]: isSelected },
+                            [
+                                currentStarsCount >= starNumber
+                                    ? s.hovered
+                                    : s.normal,
+                            ]
+                        )}
+                        key={starNumber}
+                        Svg={StarIcon}
+                        width={size}
+                        height={size}
+                        onMouseLeave={onLeave}
+                        onMouseEnter={onHover(starNumber)}
+                        onClick={onClick(starNumber)}
+                        data-testid={`StarRating.${starNumber}`}
+                        data-selected={currentStarsCount >= starNumber}
+                    />
+                );
+            })}
+        </div>
+    );
+});
