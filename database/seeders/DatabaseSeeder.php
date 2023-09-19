8a99b2e294f37b92a498c75cec96bf5d735539c9
Foxsiny <mitiashkinata8@gmail.com>
2023-09-19 16:23:03 +0300

    add reviews_of_companies_table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             CategoryVacancySeeder::class,
             CompanySeeder::class,
+            ReviewsOfCompanySeeder::class,
         ]);
     }
 }
