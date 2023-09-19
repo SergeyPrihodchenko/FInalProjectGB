c30592759dcc38818962eb43739fd6f80c69cb94
Sergo <S.prihodchenko@yandex.ru>
2023-09-19 12:45:23 +0300

    a

--- app/Enums/Experience.php
+++ app/Enums/Experience.php
@@ -4,20 +4,18 @@ namespace App\Enums;
 
 enum Experience: string
 {
-    case doesnt_matter = 'Не имеет значения';
-    case noExperience = 'Нет опыта';
-    case one_three = '1-3 года';
-    case three_six = '3-6 лет';
-    case from_six = 'более 6 лет';
+    case null = '0';
+    case one = '1';
+    case three = '3';
+    case six = '6';
 
     public static function all(): array
     {
         return [
-            self::doesnt_matter->value,
-            self::noExperience->value,
-            self::one_three->value,
-            self::three_six->value,
-            self::from_six->value,
+            self::null->value,
+            self::one->value,
+            self::three->value,
+            self::six->value,
         ];
     }
 }
