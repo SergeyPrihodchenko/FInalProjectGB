cc93aaf1d2a06272369c4711af611872eb1d6666
Valentina Saraeva <kantor-07@mail.ru>
2023-09-20 09:31:51 +0300

    update layout ResumeList and add ResumeList.module.css

--- resources/js/Pages/ResumePage/ResumeList.jsx
+++ resources/js/Pages/ResumePage/ResumeList.jsx
@@ -4,6 +4,8 @@ import MainLayout from "@/5Layouts/MainLayout/MainLayout";
 import { AppPage } from "@/5Layouts/AppPage/AppPage";
 import { AuthContext } from "@/8Shared/store/AuthContext";
 import AppLink from "@/8Shared/ui/AppLink/AppLink";
+import AppText from "@/8Shared/ui/AppText/AppText";
+import s from "./ResumeList.module.css";
 
 
 function ResumePage({resumes}){
@@ -15,18 +17,87 @@ function ResumePage({resumes}){
         <AuthContext.Provider value={{user}}>
             <MainLayout className={"app_light_theme"}>
                 <AppPage>
-                    <h2>Мои резюме</h2>
-
+                    <AppText 
+                        title={"Мои резюме"} 
+                        size="m"
+                        bold
+                        className={s.titleResumeList}
+                    />
+                    
                     {resumes.map((resume) => {
                         return (
-                            <li  className="px-4 py-2 text-black bg-gray-300 rounded">
-                                <lable><b>{resume.profession}</b></lable>
+                          
+                            <div class={s.userResume}>
+                                <div className={s.userData}>
+                                    <AppText 
+                                        title={
+                                            <span 
+                                                className={s.titleResume}>
+                                                    {resume.profession}
+                                            </span>
+                                            } 
+                                        bold 
+                                        size="s"
+                                    />
+
+                                    <AppText 
+                                        title={"90 000 ₽"} 
+                                        size="m"
+                                        bold
+                                        className={s.salaryResume}
+                                    />
+                                    <AppText 
+                                        text={"54 года"} 
+                                        size="s"
+                                    />
+                                    <AppText 
+                                        text={
+                                            <span 
+                                                className={s.skillUser}>
+                                                    Последнее место работы
+                                            </span>
+                                        } 
+                                        size="s"
+                                        
+                                    />
+                                    
+                                    <div className={s.lastSkill}>
+                                        <AppText 
+                                            text={"ИП Глизин"} 
+                                            size="s"
+                                            />
+                                        <AppText 
+                                            text={", январь 2021"} 
+                                            size="s"
+                                            />
+                                        <AppText 
+                                            text={"- по настоящее время"} 
+                                            size="s"
+                                        />
+                                    </div>
+                                    <AppText 
+                                    text={"11 лет 6 месяцев"} 
+                                    size="s"
+                                />
+                                </div>
                                 <AppLink
-                                    path={'resume.show'}
-                                    param={resume.id}
-                                    key={resume.id}
-                                > Просмотреть резюме</AppLink>
-                            </li>
+                                        path={'resume.show'}
+                                        param={resume.id}
+                                        key={resume.id}
+                                    > Просмотреть резюме</AppLink>
+                                
+                                    
+                       
+              
+                                {/* <li  className="px-4 py-2 text-black bg-gray-300 rounded">
+                                    <lable><b>{resume.profession}</b></lable> 
+                                    <AppLink
+                                        path={'resume.show'}
+                                        param={resume.id}
+                                        key={resume.id}
+                                    > Просмотреть резюме</AppLink>
+                                </li> */}
+                            </div>
                         )
                     })}
 
