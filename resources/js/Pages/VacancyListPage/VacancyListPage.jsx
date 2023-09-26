7450ec6775b5d373727bc5a614286f8eb040f3ce
Katherine <strix1112@gmail.com>
2023-09-26 13:16:36 +0300

    search vacancy by name

--- resources/js/Pages/VacancyListPage/VacancyListPage.jsx
+++ resources/js/Pages/VacancyListPage/VacancyListPage.jsx
@@ -6,17 +6,16 @@ import AppLink from "@/8Shared/ui/AppLink/AppLink";
 import AppCard from "@/8Shared/ui/AppCard/AppCard";
 import AppButton from "@/8Shared/ui/AppButton/AppButton";
 import axios from "axios";
-import Checkbox from "@/8Shared/Checkbox/Checkbox";
 import Loader from "@/8Shared/Loader/Loader";
 import s from "./VacancyListPage.module.css";
-import RadioButton from "@/8Shared/RadioButton/RadioButton";
 import cn from "classnames";
-import { Search } from "@/8Shared/Search/Search";
 import AppInput from "@/8Shared/ui/AppInput/AppInput";
-import List from "@/8Shared/List/List";
 import { Head } from "@inertiajs/react";
+import VacancyListPageFilters from "./ui/VacancyListPageFilters/VacancyListPageFilters";
+import useDebounce from "@/8Shared/Search/useDebounce";
+import List from "@/8Shared/List/List";
 
-const Vacancy = ({
+const VacancyListPage = ({
     vacancies,
     title,
     auth,
@@ -31,34 +30,49 @@ const Vacancy = ({
     const [index, setIndex] = useState(0);
     const [total, setTotal] = useState(0);
     const loaderRef = useRef(null);
+
     const [filterData, setFilterData] = useState({
         employment: [],
         schedule: [],
         experience: '',
         city_id: [],
-        title: ''
+        title: '',
+        payment: '',
     });
 
+    //поиск по названию вакансии
+    const [vacancySearchInput, setVacancySearchInput] = useState(''); // состояние инпута поиска по названию вакансии
+    const [suggestions, setSuggestions] = useState([]);
+    const debouncedVacancySearch = useDebounce(vacancySearchInput, 500);
+    const [suggestionsActive, setSuggestionsActive] = useState(false);
 
-    const [cityInput, setCityInput] = useState('');
-    const [filterCityList, setFilterCityList] = useState(cities);
-
-    const handleCityInput = (e) => {
-        const value = e.target.value;
-        setCityInput(value);
+    const handleVacancySearchInput = (e) => {
+        const { value } = e.target;
+        setVacancySearchInput(value);
 
     }
+
     useEffect(() => {
-        if (cityInput) {
-            const newList = cities.filter((city) => city.title.toLowerCase().startsWith(cityInput.toLocaleLowerCase()));
-            setFilterCityList(newList);
+        if (!debouncedVacancySearch) return;
 
-        } else {
-            setFilterCityList(cities);
-        }
+        axios.get(`/searchSort?str=${debouncedVacancySearch}`)
+            .then((res) => {
+                setSuggestions(res.data);
+            })
+            .catch((err) => console.log(err))
+        setFilterData((prevState) => {
+            return {
+                ...prevState,
+                title: debouncedVacancySearch
+            }
+        })
 
-    }, [cityInput]);
-
+    }, [debouncedVacancySearch]);
+    const handleSuggestionClick = (e) => {
+        setSuggestions([]);
+        setVacancySearchInput(e.target.innerText);
+        setSuggestionsActive(false);
+    }
 
 
     const fetchVacancyCards = useCallback(async () => {
@@ -173,7 +187,6 @@ const Vacancy = ({
                 setVacancyList(data);
                 setTotal(response.data.total);
                 setIndex(2);
-                console.log(filterData);
                 console.log(data);
             }
             getFilterData();
@@ -181,103 +194,49 @@ const Vacancy = ({
 
     }, [filterData]);
 
-    console.log('total', total);
-
     return (
         <MainLayout className={"app_light_theme"}>
             <Head title="Вакансии" />
             <AppPage>
-                <Search width={'500px'} filterChange={setValueChange} />
+                {/* <Search width={'500px'} filterChange={setValueChange} /> */}
+                <div className={s.filterSearchVacancy}>
+                    <form action="" className={s.vacancySearch}>
+                        <AppInput
+                            autocomplete="off"
+                            width={'550px'}
+                            name={'title'}
+                            placeholder={'Поиск вакансии'}
+                            value={vacancySearchInput}
+                            onChange={handleVacancySearchInput}
+                            onClick={() => setSuggestionsActive(!suggestionsActive)}
+                        />
+                    </form>
+                    {suggestionsActive &&
+                        <List
+                            className={s.serachFilterSuggestions}
+                            list={suggestions}
+                            renderItem={(suggestion) =>
+                                <li className={s.serachFilterSuggestion} onClick={handleSuggestionClick}>
+                                    {suggestion.title}
+                                </li>
+                            }
+                        />
+                    }
+
+                </div>
                 <div className={s.vacancyWrapper}>
-                    <div className={s.filterContainer}>
-                        <form action="">
-                            <AppText
-                                text="Тип занятости"
-                                bold
-                                className={s.vacancyFilterTitle}
-                            />
-                            <List
-                                list={employment}
-                                renderItem={(item) =>
-                                    <li key={item}>
-                                        <Checkbox
-                                            label={item}
-                                            name={'employment'}
-                                            checkHandler={handleChange}
-                                            value={item}
-                                        />
-                                    </li>
-                                }
-                            />
-                            <AppText
-                                text="Опыт работы"
-                                bold
-                                className={s.vacancyFilterTitle}
-                            />
-                            <RadioButton
-                                name={'experience'}
-                                label={'Не имеет значения'}
-                                value={''}
-                                onChange={handleChange} />
-
-                            <List
-                                list={experience}
-                                renderItem={(item) =>
-                                    <RadioButton
-                                        key={item}
-                                        label={item}
-                                        name={'experience'}
-                                        onChange={handleChange}
-                                        value={item}
-                                    />}
-
-                            />
-                            <AppText
-                                text="График работы"
-                                bold
-                                className={s.vacancyFilterTitle}
-                            />
-                            <List
-                                list={schedule}
-                                renderItem={(item) =>
-                                    <Checkbox
-                                        key={item}
-                                        label={item}
-                                        name={'schedule'}
-                                        checkHandler={handleChange}
-                                        value={item}
-                                    />}
-
-
-                            />
-                            <AppText
-                                text="Город"
-                                bold
-                                className={s.vacancyFilterTitle}
-                            />
-                            <AppInput
-                                width={'auto'}
-                                className={s.citiesInput}
-                                value={cityInput}
-                                onChange={handleCityInput}
-                            />
-                            <List
-                                className={s.citiesList}
-                                list={filterCityList}
-                                renderItem={(city) =>
-                                    <Checkbox
-                                        key={city.id}
-                                        label={city.title}
-                                        name={'city_id'}
-                                        value={city.id}
-                                        checkHandler={handleChange}
-                                    />
-                                }
-                            />
-                        </form>
-                    </div>
+                    <VacancyListPageFilters
+                        employment={employment}
+                        cities={cities}
+                        schedule={schedule}
+                        experience={experience}
+                        handleChange={handleChange}
+                    />
 
                     <div className={s.vacancyList}>
+                        {vacancyList.length > 0 ?
+                            <AppText bold text={`Найдено ${total} вакансии`} /> :
+                            <AppText bold text={`Ничего не найдено`} />}
                         {vacancyList.map(vac =>
                             <AppLink
                                 path={'vacancy.show'}
@@ -329,7 +288,7 @@ const Vacancy = ({
 
                 </div>
             </AppPage>
-        </MainLayout>
+        </MainLayout >
     );
 };
-export default Vacancy;
+export default VacancyListPage;
