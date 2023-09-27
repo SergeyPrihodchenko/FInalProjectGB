7f5015ba3fc3e6572a6f39e809de20fdb88bb450
Valentina Saraeva <kantor-07@mail.ru>
2023-09-27 21:31:31 +0300

    add data from database to ResumePage and ResumeList

--- resources/js/Pages/ResumeList/ResumeList.jsx
+++ resources/js/Pages/ResumeList/ResumeList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { usePage } from "@inertiajs/react";
+import { useForm, usePage } from "@inertiajs/react";
 
 import { AppPage } from "@/5Layouts/AppPage/AppPage";
 import { AuthContext } from "@/8Shared/store/AuthContext";
@@ -23,9 +23,45 @@ function ResumePage({ resumes }) {
                             className={s.titleResumeList}
                         />
 
-                        {resumes.map((resume) => {
+                        {
+                            resumes.map((resume) => {
+                                const { data } = useForm({
+                                    // user_id: user.id,
+                                    profession: resume.profession,
+                                    // first_name: resume.first_name,
+                                    // last_name: resume.last_name,
+                                    // gender: resume.gender,
+                                    // region: resume.region,
+                                    date_of_birth: resume.date_of_birth,
+                                    // phone: resume.phone,
+                                    // citizenship: resume.citizenship,
+                                    // work_permit: resume.work_permit,
+                                    // education: resume.education,
+                                    // educational_institute: resume.educational_institute,
+                                    companies: resume.companies,
+                                    // skills: resume.skills,
+                                    // experience: resume.experience,
+                                });
+                            
+                                //высчитываем из даты рождения сколько полных лет
+                                const dateOfBirth = data.date_of_birth;
+                                function declOfNum(number, titles) {
+                                   let cases = [2, 0, 1, 1, 1, 2];
+                                    return number + " " + titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+                                  }
+                                  
+                                  function birthDateToAge(b) {
+                                    var n = new Date(),
+                                        b = new Date(b),
+                                        age = n.getFullYear() - b.getFullYear();
+                                    return n.setFullYear(1970) < b.setFullYear(1970) ? age - 1 : age;
+                                  }
+                                  const Yers = (declOfNum(birthDateToAge(dateOfBirth), ['год', 'года', 'лет']));
+                                //  console.log(Yers);
+
                             return (
                                 <div class={s.resumeList}>
+                                    
                                     <div class={s.userResume}>
                                         <div className={s.userData}>
                                             <AppText
@@ -49,9 +85,10 @@ function ResumePage({ resumes }) {
                                                 className={s.salaryResume}
                                             />
                                             <AppText
-                                                text={"54 года"}
+                                                text={Yers}
                                                 size="xs"
                                             />
+                                            
                                             <AppText
                                                 text={
                                                     <span
@@ -62,7 +99,15 @@ function ResumePage({ resumes }) {
                                                 }
                                                 size="xs"
                                             />
-
+{/* не могу прокинуть данные по компании вообще и по последней в частности: название, стаж работы */}
+                                {/* {
+                                    data.companies.map((el) => {
+                                        return (
+                                            console.log(el.name)
+                                        )
+                                    })
+                                } */}
+                                            
                                             <div className={s.lastSkill}>
                                                 <AppText
                                                     text={"ИП Глизин"}
@@ -79,6 +124,9 @@ function ResumePage({ resumes }) {
                                                     size="xs"
                                                 />
                                             </div>
+
+                                                    
+
                                             <AppText
                                                 text={"11 лет 6 месяцев"}
                                                 size="xs"
@@ -111,9 +159,7 @@ function ResumePage({ resumes }) {
                                             Редактировать резюме
                                         </AppButton>
 
-                                        {/* Сделать активной кнопку редаткировать 
-                                        только для пользователя чье резюме открыто
-                                        */}
+{/* Сделать активной кнопку редаткировать только для пользователя чье резюме открыто */}
                                     </div>
                                 </div>
                             );
