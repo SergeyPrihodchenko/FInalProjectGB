abf3d8c10490eea7644e822d876da8609ffea13a
Sergo <S.prihodchenko@yandex.ru>
2023-09-27 10:54:15 +0300

    a

--- resources/js/Pages/Company/create_company.jsx
+++ resources/js/Pages/Company/create_company.jsx
@@ -59,69 +59,69 @@ const Company = ({ auth, cities }) => {
                     <input id="description" value={data.description} onChange={e => setData('description', e.target.value)} />
 
 
-//                     <input
-//                         id="email"
-//                         value={data.email}
-//                         onChange={(e) => setData("email", e.target.value)}
-//                     />
-//                     <label htmlFor="name">Имя:</label>
-//                     <input
-//                         id="name"
-//                         value={data.name}
-//                         onChange={(e) => setData("name", e.target.value)}
-//                     />
-//                     <label htmlFor="business_profile">
-//                         Сфера деятельности:
-//                     </label>
-//                     <input
-//                         id="business_profile"
-//                         value={data.business_profile}
-//                         onChange={(e) =>
-//                             setData("business_profile", e.target.value)
-//                         }
-//                     />
-//                     <label htmlFor="website">Сайт:</label>
-//                     <input
-//                         id="website"
-//                         value={data.website}
-//                         onChange={(e) => setData("website", e.target.value)}
-//                     />
-//                     <label htmlFor="region_of_location">
-//                         Город или регион расположения:
-//                     </label>
-//                     <input
-//                         id="region_of_location"
-//                         value={data.region_of_location}
-//                         onChange={(e) =>
-//                             setData("region_of_location", e.target.value)
-//                         }
-//                     />
-//                     <label htmlFor="date_create">Дата основания:</label>
-//                     <input
-//                         type="date"
-//                         id="start"
-//                         name="trip-start"
-//                         value={data.date_create}
-//                         onChange={(e) => setData("date_create", e.target.value)}
-//                     />
-//                     <label htmlFor="phone_number">
-//                         Контактный номер телефона:
-//                     </label>
-//                     <input
-//                         id="phone_number"
-//                         value={data.phone_number}
-//                         onChange={(e) =>
-//                             setData("phone_number", e.target.value)
-//                         }
-//                     />
-//                     <label htmlFor="description">
-//                         Раскажите о всоей компании:
-//                     </label>
-//                     <input
-//                         id="description"
-//                         value={data.description}
-//                         onChange={(e) => setData("description", e.target.value)}
-//                     />
+{/*                     <input
+                         id="email"
+                         value={data.email}
+                         onChange={(e) => setData("email", e.target.value)}
+                     />
+                     <label htmlFor="name">Имя:</label>
+                     <input
+                         id="name"
+                         value={data.name}
+                         onChange={(e) => setData("name", e.target.value)}
+                     />
+                     <label htmlFor="business_profile">
+                         Сфера деятельности:
+                     </label>
+                     <input
+                         id="business_profile"
+                         value={data.business_profile}
+                         onChange={(e) =>
+                             setData("business_profile", e.target.value)
+                         }
+                     />
+                     <label htmlFor="website">Сайт:</label>
+                     <input
+                         id="website"
+                         value={data.website}
+                         onChange={(e) => setData("website", e.target.value)}
+                     />
+                     <label htmlFor="region_of_location">
+                         Город или регион расположения:
+                     </label>
+                     <input
+                         id="region_of_location"
+                         value={data.region_of_location}
+                         onChange={(e) =>
+                             setData("region_of_location", e.target.value)
+                         }
+                     />
+                     <label htmlFor="date_create">Дата основания:</label>
+                     <input
+                         type="date"
+                         id="start"
+                         name="trip-start"
+                         value={data.date_create}
+                         onChange={(e) => setData("date_create", e.target.value)}
+                     />
+                     <label htmlFor="phone_number">
+                         Контактный номер телефона:
+                     </label>
+                     <input
+                         id="phone_number"
+                         value={data.phone_number}
+                         onChange={(e) =>
+                             setData("phone_number", e.target.value)
+                         }
+                     />
+                     <label htmlFor="description">
+                         Раскажите о всоей компании:
+                     </label>
+                     <input
+                         id="description"
+                         value={data.description}
+                         onChange={(e) => setData("description", e.target.value)}
+                     /> */}
 
                     <button type="submit">Отправить</button>
                 </form>
