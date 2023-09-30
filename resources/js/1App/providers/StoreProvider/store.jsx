f03f3f6f0fbdc4ee1cf64e8965bf66697000659c
Valentina Saraeva <kantor-07@mail.ru>
2023-09-30 14:24:14 +0300

    test toolkit

--- resources/js/1App/providers/StoreProvider/store.jsx
+++ resources/js/1App/providers/StoreProvider/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../counterSlice/counterSlice";
+import resumePageSlice from "@/Pages/ResumeCreate/model/resumePageSlice";
 
 export const createReduxStore = ({ asyncReducers }) => {
     const store = configureStore({
         reducer: {
             counter: counterReducer,
+            resumePage:resumePageSlice,
         },
     });
     return store;
