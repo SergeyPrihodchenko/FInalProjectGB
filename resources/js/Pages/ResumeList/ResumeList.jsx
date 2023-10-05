b0c2576e3993a520193b6a3b90ee54b86733472b
Valentina Saraeva <kantor-07@mail.ru>
2023-10-05 18:55:09 +0300

    creaded filter of ResumeList

--- resources/js/Pages/ResumeList/ResumeList.jsx
+++ resources/js/Pages/ResumeList/ResumeList.jsx
@@ -6,12 +6,58 @@ import { AuthContext } from "@/8Shared/store/AuthContext";
 import AppText from "@/8Shared/ui/AppText/AppText";
 import s from "./ResumeList.module.css";
 import AppButton from "@/8Shared/ui/AppButton/AppButton";
+import RadioButton from "@/8Shared/RadioButton/RadioButton";
+import Checkbox from "@/Components/Checkbox";
+import AppInput from "@/8Shared/ui/AppInput/AppInput";
 
 function ResumePage({ resumes }) {
     const user = usePage().props.auth.user;
 
-    console.log(resumes);
+//    console.log(resumes);
+const arrayEducation = [
+    "Среднее",
+    "Среднее специальное",
+    "Неоконченное высшее",
+    "Высшее образование",
+    "Бакалавр",
+    "Магистр",
+    "Кандидат наук",
+];
 
+const arrayExperience = [
+    "нет опыта",
+    "от 1 до 3 лет",
+    "от 3 до 6 лет",
+    "более 6 лет",
+];
+
+const arrayEmployment = [
+    "Полный день",
+    "Сменный график",
+    "Гибкий график",
+    "Удаленная работа",
+    "Вахтовый метод"
+];
+
+const arraySchedule = [
+    "Полная занятость",
+    "Частичная занятость",
+    "Стажировка",
+    "Проектная работа",
+    "Волонтерство"
+];
+
+const arrayRelocation = [
+    "Невозможно",
+    "Возможно",
+    "Желательно",
+];
+
+const arrayBuisnessTravel = [
+    "Никогда",
+    "Готов",
+    "Иногда",
+];
     return (
         <AuthContext.Provider value={{ user }}>
             <>
@@ -54,52 +100,192 @@ function ResumePage({ resumes }) {
                                 //  console.log(Yers);
 
                             return (
-                                <div class={s.resumeList}>
-                                    <div class={s.userResume}>
-                                        <div className={s.userData}>
-                                            <div className={s.buttonResumeProfession}>
-                                                <AppButton
-                                                    path={"resume.show"}
-                                                    param={resume.id}
-                                                    key={resume.id}
-                                                    sizeText="s"
-                                                    bold
-                                                    variant="clear"
-                                                    colorType="accent"
-                                                >{resume.profession}
-                                                </AppButton>
-                                            </div>
 
+                                <div className={s.resumeListPage}>
+                                     <div className={s.vacancyWishes}>
+                                        <div className={s.education}>
                                             <AppText
-                                                title={resume.salary.concat(" " , "₽")}
-                                                size="s"
+                                                title={"Тип занятости"}
                                                 bold
-                                                className={s.salaryResume}
+                                                size={"s"}
+                                                className={s.textTitle}
                                             />
-                                            <AppText
-                                                text={Yers}
-                                                size="xs"
-                                            />
-
-                                            <AppText
-                                                text={"Регион: ".concat(resume.region)}
-                                                size="xs"
-                                            />
-                                                                                           
-                                            <AppText
-                                                text={"Опыт работы: ".concat(resume.experience)}
-                                                size="xs"
-                                            />
-                                            
-                                            <AppText
-                                                text={"Образование: ".concat(resume.education)}
-                                                size="xs"
-                                            />
+                                            {arraySchedule.map((el) => {
+                                                // console.log(el);
+                                                return (
+                                                    <div className={s.checkboxResume}>
+                                                        <Checkbox
+                                                            name="employment_type"
+                                                            id={el}
+                                                            value={el}
+                                                            className={s.checkboxResumeEl}
+                                                        />
+                                                        <label for={el}>
+                                                            <AppText
+                                                                text={el}
+                                                                size={"s"}
+                                                                className={s.checkboxResumeText}
+                                                            />
+                                                        </label>
+                                                    </div>
+                                                )
+                                            })}
                                         </div>
                                         
-                                        <div class={s.userPhoto}>
-                                            НЕТ ФОТО
-                                            {/* <img src="#" className={s.imgUserPhoto}/> */}
+                            
+                                        <div className={s.education}>
+                                            <AppText
+                                                title={"Опыт работы"}
+                                                bold
+                                                size={"s"}
+                                                className={s.textTitle}
+                                            />
+                                            
+                                            {arrayExperience.map((el) => {
+                                                // console.log(el);
+                                                return (
+                                                    <div className={s.inputRadioBasiceData}>    
+                                                        <RadioButton
+                                                            label={el}
+                                                            value={el}
+                                                            className={s.inputResume}
+                                                            />
+                                                    </div>
+                                                );
+                                            })}
+                                        </div>
+                                        <AppText
+                                            title={"Уровень дохода"}
+                                            bold
+                                            size={"s"}
+                                            className={s.textTitle}
+                                        />
+                                        <AppInput    
+                                            type="text"
+                                            placeholder="от 100000"
+                                            width={"220px"}    
+                                        />
+                                        <div className={s.education}>
+                                            <AppText
+                                                title={"График работы"}
+                                                bold
+                                                size={"s"}
+                                                className={s.textTitle}
+                                            />
+
+                                            {arrayEmployment.map((el) => {
+                                                // console.log(el);
+                                                return (
+                                                    <div className={s.checkboxResume}>
+                                                        <Checkbox
+                                                            name="schedule_type"
+                                                            id={el}
+                                                            value={el}
+                                                            className={s.checkboxResumeEl}
+                                                        />
+                                                        <label for={el}>
+                                                            <AppText
+                                                                text={el}
+                                                                size={"s"}
+                                                                className={s.checkboxResumeText}
+                                                            />
+                                                        </label>
+                                                    </div>
+                                                )
+                                            })}
+
+                                        </div>
+                                        <div className={s.education}>
+                                            <AppText
+                                                title={"Готовность к переездам"}
+                                                bold
+                                                size={"s"}
+                                                className={s.textTitle}
+                                            />
+                                            {arrayRelocation.map((el) => {
+                                                // console.log(el);
+                                                return (
+                                                    <div className={s.inputRadioBasiceData}> 
+                                                        <RadioButton
+                                                            name="relocation"
+                                                            label={el}
+                                                            value={el} 
+                                                            className={s.inputResume}
+                                                        />
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                        <div className={s.education}>
+                                            <AppText
+                                                title={"Готовность к командировкам"}
+                                                bold
+                                                size={"xs"}
+                                                className={s.textTitle}
+                                            />
+                                            {arrayBuisnessTravel.map((el) => {
+                                                // console.log(el);
+                                                return (
+                                                    <div className={s.inputRadioBasiceData}> 
+                                                        <RadioButton
+                                                            name="buisness_travel"
+                                                            label={el}
+                                                            value={el} 
+                                                            className={s.inputResume}
+                                                        />
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+
+                                    <div class={s.resumeList}>
+                                        <div class={s.userResume}>
+                                            <div className={s.userData}>
+                                                <div className={s.buttonResumeProfession}>
+                                                    <AppButton
+                                                        path={"resume.show"}
+                                                        param={resume.id}
+                                                        key={resume.id}
+                                                        sizeText="s"
+                                                        bold
+                                                        variant="clear"
+                                                        colorType="accent"
+                                                    >{resume.profession}
+                                                    </AppButton>
+                                                </div>
+
+                                                <AppText
+                                                    title={resume.salary.concat(" " , "₽")}
+                                                    size="s"
+                                                    bold
+                                                    className={s.salaryResume}
+                                                />
+                                                <AppText
+                                                    text={Yers}
+                                                    size="xs"
+                                                />
+
+                                                <AppText
+                                                    text={"Регион: ".concat(resume.region)}
+                                                    size="xs"
+                                                />
+                                                                                            
+                                                <AppText
+                                                    text={"Опыт работы: ".concat(resume.experience)}
+                                                    size="xs"
+                                                />
+                                                
+                                                <AppText
+                                                    text={"Образование: ".concat(resume.education)}
+                                                    size="xs"
+                                                />
+                                            </div>
+                                            
+                                            <div class={s.userPhoto}>
+                                                НЕТ ФОТО
+                                                {/* <img src="#" className={s.imgUserPhoto}/> */}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
