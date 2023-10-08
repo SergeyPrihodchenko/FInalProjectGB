296e360b8e8b374502261272fdecd5f361806dd1
Sergey <S.prihodchenko@yandex.ru>
2023-10-08 21:50:43 +0300

    a

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-       '/vacancy',
-       '/vacancy/*',
-        '/company/*'
+        '/vacancy',
+        '/vacancy/*',
+        '/company/*',
+        '/addLike'
     ];
 }
