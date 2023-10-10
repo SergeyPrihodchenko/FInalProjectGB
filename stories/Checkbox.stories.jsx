72455e342ce2b0a58cf51b2f9fde1f456920b1ca
starscy <starscy@rambler.ru>
2023-10-10 22:12:15 +0300

    add storybook

---
+++ stories/Checkbox.stories.jsx
@@ -0,0 +1,19 @@
+import Checkbox from "../resources/js/Components/Checkbox";
+
+export default {
+    title: 'GoodJob/Checkbox',
+    component: Checkbox,
+    argTypes: {
+    }
+}
+
+const Template = (arg) => <Checkbox {...arg}/>;
+
+export const Default = Template.bind({});
+Default.args = {
+
+};
+
+
+
+
