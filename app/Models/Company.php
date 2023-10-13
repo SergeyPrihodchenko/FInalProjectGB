55b51a5211683c4567f9006b5e13f44becdb5fc4
Foxsiny <mitiashkinata8@gmail.com>
2023-10-13 11:08:31 +0300

    changes in the Model Company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -44,7 +44,10 @@ class Company extends Model
     {
         return $this->belongsTo(User::class, 'creator_id');
     }
-
+    public function getLogoUrl()
+    {
+        return asset('storage/' . $this->logo);
+    }
 
 
 }
